198775447a0ccb6eab6db37c53e86b5d643e9de1
4hmedsalah <ahmedsalah.tech@proton.me>
2025-05-28 22:25:17 +0300

    Solve title-case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(str) {
+    return str
+        .split(" ") // Split the sentence into words
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize the first letter of each word
+        .join(" "); // Join the words back into a sentence
+}
 
 module.exports = titleCase;
