cd2fd2c0b39d8ba04d45590e5d002252ba9286bb
4hmedsalah <ahmedsalah.tech@proton.me>
2025-05-28 22:04:24 +0300

    Solve find-max-number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+const findMaxNumber = (arr) => {
+    if (!Array.isArray(arr)) {
+        throw new Error("Input must be an array");
+    }
+
+    if (arr.length === 0) {
+        throw new Error("Array must not be empty");
+    }
+    return Math.max(...arr);
+};
 
 module.exports = findMaxNumber;
